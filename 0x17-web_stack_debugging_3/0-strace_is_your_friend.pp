ff773143f259ced15b8c495af5dd7c00911f968e
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-12-06 11:13:04 -0800

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+#puppet manifest to rename a file and create a file
+
+exec { 'fix-wordpress':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
+}
